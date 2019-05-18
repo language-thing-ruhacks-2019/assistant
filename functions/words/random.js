063d39408181ee8a85408c02cc8d9991fb4b5152
Ronny Chan <ronny6993@gmail.com>
2019-05-18 17:06:41 -0400

    add random

--- functions/words/random.js
+++ functions/words/random.js
@@ -1,13 +1,17 @@
-const fs = require('fs');
-const nthline = require('nthline');
+const wordTable = require('./words.json');
 
-const toInteger = (num) => {
-    
+const getKey = (num) => {
+    if (num === 1) return 'a1';
+    if (num === 2) return 'a2';
+    if (num === 3) return 'b1';
+    if (num === 4) return 'b2';
+    if (num === 5) return 'c1';
+    if (num === 6) return 'c2';
+    return 'a1';
+}
+const words = (index) => {
+    const key = getKey(index);
+    return wordTable[key][Math.floor(Math.random() * wordTable[key].length)]
 }
 
-const rand = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-const words = async (filename) => {
-    return nthline(row, filename);
-}
+module.exports = words;
